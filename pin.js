e5fa12461fb700e2c24a6c537af3d3594ed8ae25
TFMahabub <rjmahabub543@gmail.com>
2022-08-21 09:31:32 +0600

    code done

--- pin.js
+++ pin.js
@@ -13,7 +13,7 @@ function getPin(){
 }
 
 
-//Generate-button-
+//Generate code button-
 document.getElementById('generate-button').addEventListener('click', function(){
   //call the function to getPin-
   const randomNum = getPin();
@@ -24,12 +24,11 @@ document.getElementById('generate-button').addEventListener('click', function(){
 })
 
 
-//funtion for calculator-
+//calculator buttons-
 document.getElementById('calculator').addEventListener('click', function(event){
   const numbers = event.target.innerText;
   
-  
-
+  //diclare the calculator input value-
   const inputValue = document.getElementById('input-value-calculator');
   const inputValueText = inputValue.value;
 
@@ -50,5 +49,25 @@ document.getElementById('calculator').addEventListener('click', function(event){
     //get the input string value and plus the value-
     let sumString = inputValueText + numbers;
     inputValue.value = sumString;
+  }
+})
+
+
+
+//submit button-
+document.getElementById('submit-button').addEventListener('click', function(){
+  const codeGenerateInput = document.getElementById('input-value').value;
+  const calculatorInput = document.getElementById('input-value-calculator').value;
+
+  //condition-
+  if(calculatorInput === ''){
+    alert('please type the code');
+  }
+  else if(codeGenerateInput === calculatorInput){
+
+    console.log('code matched');
+  }
+  else{
+    console.log('code not matched')
   }
 })
